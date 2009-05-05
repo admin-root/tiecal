280b4dff0eb0b1e5c6862051d318f2aa8e0491c8
isak.savo <isak.savo@d8e3a3bc-2c27-11de-8e0c-53a27eea117e>
2009-05-05 19:09:15 +0000

    first shot at parallelizing the merging of calendar entries GUI changed to show each step as a box with separate progressbar - show the natural flow of the worksteps

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Text;
 
 namespace TieCal
 {
@@ -12,5 +13,57 @@ namespace TieCal
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += new System.Windows.Threading.DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+        }
+
+        private string GetExceptionDetailsString(Exception ex, int indent)
+        {
+            StringBuilder sb = new StringBuilder();
+            string padding = new string(' ', indent);
+            sb.AppendFormat("{0}Type: {1}", padding, ex.GetType().FullName);
+            sb.AppendLine();
+            sb.AppendFormat("{0}Message: {1}", padding, ex.Message);
+            sb.AppendLine();
+            sb.AppendLine(padding + "Details:");
+            sb.Append(padding + padding);
+            if (ex is System.IO.FileNotFoundException)
+                sb.AppendFormat("FileName: {0}", (ex as System.IO.FileNotFoundException).FileName);
+            else if (ex is ArgumentException)
+                sb.AppendFormat("Parameter: {0}", padding, (ex as ArgumentException).ParamName);
+            return sb.ToString();
+        }
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder("Unhandled Exception: ");
+                if (e.Exception != null)
+                {
+                    sb.AppendLine(GetExceptionDetailsString(e.Exception, 0));
+                    Exception ex = e.Exception.InnerException;
+                    int indent = 2;
+                    while (ex != null)
+                    {
+                        sb.AppendLine(GetExceptionDetailsString(ex, indent));
+                        ex = ex.InnerException;
+                        indent += 2;
+                    }
+                    sb.AppendLine();
+                    sb.AppendLine("StackTrace: " + e.Exception.StackTrace);
+                }
+                MessageBox.Show(sb.ToString());
+
+            }
+            catch
+            {
+                /* We can't allow a throw in this method */
+            }
+            finally
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
