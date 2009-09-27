212e915e44d068b8d528ca90a93070f1a1b3359e
isak.savo <isak.savo@d8e3a3bc-2c27-11de-8e0c-53a27eea117e>
2009-09-27 18:40:36 +0000

    Add copyright header

--- trunk/SetupRemindersDialog.xaml.cs
+++ trunk/SetupRemindersDialog.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// Part of the TieCal project (http://code.google.com/p/tiecal/)
+// Copyright (C) 2009, Isak Savo <isak.savo@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//      http://www.gnu.org/licenses/gpl.html
+using System;
 using System.IO;
 using System.Net;
 using System.Windows;
