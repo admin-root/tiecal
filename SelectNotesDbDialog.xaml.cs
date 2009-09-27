24ea35c00c81fbebc90e32c328404f61a5f77577
isak.savo <isak.savo@d8e3a3bc-2c27-11de-8e0c-53a27eea117e>
2009-09-27 17:03:32 +0000

    New dialogs for configuring notes database + reminder settings

---
+++ SelectNotesDbDialog.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace TieCal
+{
+	public partial class SelectNotesDbDialog
+	{
+		public SelectNotesDbDialog()
+		{
+			this.InitializeComponent();
+		}
+
+        public SelectNotesDbDialog(NotesReader notesReader) : this()
+        {
+            RefreshNotesDatabases(notesReader);
+        }
+
+        private void RefreshNotesDatabases(NotesReader notesReader)
+        {
+            cmbNotesDB.Items.Clear();
+            cmbNotesDB.ItemsSource = notesReader.GetAvailableDatabases();
+            if (ProgramSettings.Instance.NotesDatabase != null)
+                cmbNotesDB.SelectedItem = ProgramSettings.Instance.NotesDatabase;
+            else
+            {
+                // Make a default selection. The one with the calendar is most often the one named: mail\<username>.nsf
+                foreach (var item in cmbNotesDB.Items)
+                {
+                    if (item.ToString().StartsWith(@"mail\") && item.ToString().EndsWith(".nsf"))
+                    {
+                        cmbNotesDB.SelectedItem = item;
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the notes database that the user selected.
+        /// </summary>
+        public string NotesDatabase
+        {
+            get { return (string)cmbNotesDB.SelectedItem; }
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            //Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            //Close();
+        }
+	}
+}
---
+++ SetupRemindersDialog.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace TieCal
+{
+	public partial class SetupRemindersDialog
+	{
+		public SetupRemindersDialog()
+		{
+			this.InitializeComponent();			
+			// Insert code required on object creation below this point.
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this) == false)
+            {
+                ReminderMode = ProgramSettings.Instance.ReminderMode;
+                if (ReminderMode == ReminderMode.Custom)
+                    ReminderMinutes = ProgramSettings.Instance.ReminderMinutesBeforeStart;
+                else
+                    ReminderMinutes = 15;
+            }
+		}
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            //Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            //Close();
+        }
+
+        public ReminderMode ReminderMode
+        {
+            get
+            {
+                if (rdoOutlook.IsChecked == true)
+                    return ReminderMode.OutlookDefault;
+                else if (rdoCustom.IsChecked == true)
+                    return ReminderMode.Custom;
+                else if (rdoDisable.IsChecked == true)
+                    return ReminderMode.NoReminder;
+                else
+                    throw new InvalidOperationException("No reminder has been selected");
+            }
+            set
+            {
+                switch (value)
+                {
+                    case ReminderMode.NoReminder:
+                        rdoDisable.IsChecked = true;
+                        break;
+                    case ReminderMode.OutlookDefault:
+                        rdoOutlook.IsChecked = true;
+                        break;
+                    case ReminderMode.Custom:
+                        rdoCustom.IsChecked = true;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Reminder mode " + value + " is not known");
+                }
+            }
+        }
+
+        public int ReminderMinutes
+        {
+            // TODO: error checking
+            get { return Int32.Parse(txtMinutes.Text); }
+            set { txtMinutes.Text = value.ToString(); }
+        }
+	}
+}
