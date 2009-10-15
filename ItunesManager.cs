97b4b5157dc46f8d056c08703eb3db1770d678c6
isak.savo <isak.savo@d8e3a3bc-2c27-11de-8e0c-53a27eea117e>
2009-10-15 14:13:12 +0000

    Add support for syncing directly to the iPhone (using iTunes COM interface). Not perfect yet, but it does the job)

---
+++ ItunesManager.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TieCal
+{
+    class ItunesManager
+    {
+        public ItunesManager()
+        {
+            SynchronizeWorker = new BackgroundWorker();
+            SynchronizeWorker.DoWork += new DoWorkEventHandler(SynchronizeWorker_DoWork);
+            SynchronizeWorker.WorkerSupportsCancellation = false;
+            SynchronizeWorker.WorkerReportsProgress = true;
+        }
+
+        private IITIPodSource GetIpodById(iTunesApp app, ItunesId IphoneId)
+        {
+            foreach (IITSource src in app.Sources)
+            {
+                if (src.Kind == ITSourceKind.ITSourceKindIPod)
+                {
+                    object o = src;
+                    ItunesId id = new ItunesId(app.get_ITObjectPersistentIDHigh(ref o), app.get_ITObjectPersistentIDLow(ref o));
+                    if (id == IphoneId)
+                        return src as IITIPodSource;
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets the first connected ipod in iTunes.
+        /// </summary>
+        /// <param name="app">The itunes app object.</param>
+        /// <returns>An IPodSource or null if no ipod is connected</returns>
+        private IITIPodSource GetFirstIpod(iTunesApp app)
+        {
+            foreach (IITSource src in app.Sources)
+            {
+                if (src.Kind == ITSourceKind.ITSourceKindIPod)
+                    return src as IITIPodSource;
+            }
+            return null;
+        }
+
+        void SynchronizeWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                iTunesApp app;
+                BackgroundWorker worker = (BackgroundWorker)sender;
+                worker.ReportProgress(5);
+                app = new iTunesAppClass();
+                IITIPodSource iphone = null;
+                worker.ReportProgress(20);                
+                if (ProgramSettings.Instance.IphoneId.IsEmpty)
+                {
+                    iphone = GetFirstIpod(app);                    
+                }
+                else
+                    iphone = GetIpodById(app, ProgramSettings.Instance.IphoneId);
+                worker.ReportProgress(40);
+                if (iphone == null)
+                {
+                    // no iphone connected
+                    worker.ReportProgress(100);
+                }
+                object o = iphone;
+                ProgramSettings.Instance.IphoneId = new ItunesId(app.get_ITObjectPersistentIDHigh(ref o), app.get_ITObjectPersistentIDLow(ref o));
+                iphone.UpdateIPod();
+                for (int i = 50; i < 100; i++)
+                {
+                    worker.ReportProgress(i);
+                    Thread.Sleep(250);
+                }
+            }
+            catch (COMException ex)
+            {
+                e.Result = new ApplicationException("Failed to communicate with iTunes: " + ex.Message, ex);
+            }
+        }       
+
+        public BackgroundWorker SynchronizeWorker { get; private set; }
+    }
+
+    /// <summary>
+    /// Represents an object in iTunes. These objects have 64bit persistent ids which are divided into two parts.
+    /// </summary>
+    public struct ItunesId
+    {
+        private int _high, _low;
+        public Int32 High { get { return _high; } }
+        public Int32 Low { get { return _low; } }
+        public ItunesId(Int32 high, Int32 low) 
+        {
+            _high = high;
+            _low = low;
+        }
+
+        public bool IsEmpty
+        {
+            get { return _high == 0 && _low == 0; }
+        }
+
+        public static bool operator ==(ItunesId left, ItunesId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ItunesId left, ItunesId right)
+        {
+            return !(left == right);
+        }
+
+        public override int GetHashCode()
+        {
+            return _low.GetHashCode() ^ _high.GetHashCode();
+        }
+
+        public bool Equals(ItunesId obj)
+        {
+            return obj._low == _low && obj._high == _high;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is ItunesId && Equals((ItunesId)obj);
+        }
+    }
+}
