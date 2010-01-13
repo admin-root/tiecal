977ea73dda0abd4161ae0d28e1873387d35251f7
isak.savo <isak.savo@d8e3a3bc-2c27-11de-8e0c-53a27eea117e>
2010-01-13 12:03:07 +0000

    add excel sheet for repeat pattern analyzing fix tester output

--- trunk/Tester/Program.cs
+++ trunk/Tester/Program.cs
@@ -27,10 +27,12 @@ namespace Tester
                     while ((line = reader.ReadLine()) != null)
                     {
                         i++;
+                        if (line.Trim().Length == 0)
+                            continue;
                         var items = line.Split(';');
                         if (items.Length < 5)
                         {
-                            Console.WriteLine("Line {0} doesn't contain occurrences ({1})", i, items.Length > 0 ? items[0] : "(no subject)");
+                            Console.WriteLine("Line {0} doesn't contain occurrences ({1})", i, items.Length > 1 ? items[1] : "(no subject)");
                             continue;
                         }
                         List<DateTime> occurrences = new List<DateTime>();
@@ -42,12 +44,12 @@ namespace Tester
                         try
                         {
                             var pattern = RepeatPattern.CreateFromOccurrences(occurrences);
-                            Console.WriteLine("Test SUCCEEDED ({0})", items[0]);
+                            Console.WriteLine("Test SUCCEEDED ({0})", items[1]);
                             Console.WriteLine("Repeat pattern created from {0} occurrences: {1}", occurrences.Count, pattern.ToString());
                         }
                         catch (ArgumentException)
                         {
-                            Console.WriteLine("Test FAILED ({0})", items[0]);
+                            Console.WriteLine("Test FAILED ({0})", items[1]);
                             Console.WriteLine("Failed to create repeat pattern from {0} occurrences", occurrences.Count);
                         }
                     }
